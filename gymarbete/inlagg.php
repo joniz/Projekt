294848a3425012038fe40e6e473aff9ee3ab9b91
Mirnes Omerovic <Omerovic35@gmail.com>
2015-03-05 14:20:57 +0100

    fake commit
    aa du fattar

--- gymarbete/inlagg.php
+++ gymarbete/inlagg.php
@@ -7,10 +7,10 @@ top();
 echo"<form action=\"upload.php\" method=\"post\" enctype=\"multipart/form-data\">";
    echo"<p>Select image to upload:<p>";
     echo"<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">";
-echo"<label for=\"rubrik\">rubrik</label>";
+echo"<label for=\"rubrik\" class=\"uploadlabel\">rubrik</label>";
 echo"<input type=\"text\" name=\"rubrik\" id=\"rubrik\">";
 
-echo"<label for=\"text\">text</label>";
+echo"<label for=\"text\" class=\"uploadlabel\">text</label>";
 echo"<input type=\"textarea\" name=\"text\" id=\"text\">";
 
     echo"<input type=\"submit\" value=\"Upload Image\" name=\"submit\">";
