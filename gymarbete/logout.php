52f789c38b485b03e0e64076c04908ee6a41a064
joniz <jaej96@gmail.com>
2015-03-06 12:27:05 +0100

    Nya filer

--- gymarbete/logout.php
+++ gymarbete/logout.php
@@ -1,21 +1,5 @@
 <?php
 session_start();
-<<<<<<< HEAD
-
-if($_POST['action']=='logout'){
-
-
-unset($_SESSION['user']);
-unset($_SESSION['admin']);
-header('Location: index.php');
-}
-else{
-	echo "Nä";
-
-
-}
-=======
-	
 if(isset($_SESSION['admin'])){
 unset($_SESSION['user']);
 unset($_SESSION['admin']);    
@@ -25,5 +9,5 @@ header('Location: index.php');
 echo"Du ska inte vara här";
 }
 
->>>>>>> origin/master
+
 ?>
