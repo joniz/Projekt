58592fb56c7bbf720c58239474a7f9ab6d171d64
joniz <jaej96@gmail.com>
2015-03-06 10:07:25 +0100

    Logout-scriptet funkar bra nu

--- gymarbete/logout.php
+++ gymarbete/logout.php
@@ -4,6 +4,7 @@ session_start();
 if(isset($_SESSION['admin'])){
 unset($_SESSION['user']);
 unset($_SESSION['admin']);    
+session_destroy();
 header('Location: index.php'); 
 }else{
 echo"Du ska inte vara här";
