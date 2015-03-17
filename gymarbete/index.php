c478631809fea897f6ef2ca3ea958be6013d77bd
joniz <jaej96@gmail.com>
2015-03-17 22:36:12 +0100

    swag

--- gymarbete/index.php
+++ gymarbete/index.php
@@ -15,13 +15,15 @@ top();
 
 
 
-$stmt = $dbh->query('SELECT * FROM tbl_inlagg order by inlagg_id desc');
+$stmt = $dbh->query('SELECT * FROM tbl_inlagg ORDER BY inlagg_id DESC');
  
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-   echo '<h1>'.$row['inlagg_rubrik'].'</h1> '.$row['inlagg_text'].''.$row['inlagg_bild'];
+   
+   
+   	echo '<h1>'.$row['inlagg_rubrik'].'</h1></<br> '.$row['inlagg_text'].'</br>'.$row['inlagg_bild'];
+
 
 }
-
 
 
 
