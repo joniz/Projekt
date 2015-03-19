5636ac36b6554e73afa453689b0f26bd906b6e67
joniz <jaej96@gmail.com>
2015-03-19 15:31:40 +0100

    bajs

--- gymarbete/upload.php
+++ gymarbete/upload.php
@@ -1,13 +1,13 @@
 <?php
-if (isset(f_bild)) {
-    # code...
+
+    
 
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+if(isset($_GET["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
@@ -16,7 +16,7 @@ if(isset($_POST["submit"])) {
         echo "File is not an image.";
         $uploadOk = 0;
     }
-}
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
