81917059f4cd21b32f760303bb4d2287c54937a1
joniz <jaej96@gmail.com>
2015-03-23 23:41:26 +0100

    sweg

--- gymarbete/formularinlagg.php
+++
@@ -1,9 +0,0 @@
-<form action="inlaggscript.php" method="POST" enctype="multipart/form-data">
-
-<input type="hidden" name="page" value="test">
-
-<p>Rubrik</p> <input type="text" name="f_rubrik"/>
-<p>Text</p> <textarea type="text" name="f_text"></textarea>
-<p>Bild</p> <input type="file" id="fileToUpload" name="fileToUpload"/>
-<input type="submit" name="submit">
-</form>
